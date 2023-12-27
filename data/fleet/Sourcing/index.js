a54ae4bf79892ad41e5d9f2258c6174d9375cb96
fullname <fullname@fullnames-MacBook-Pro.local>
2023-12-27 14:56:07 -0800

    i

--- data/fleet/Sourcing/index.js
+++ data/fleet/Sourcing/index.js
@@ -1,23 +1,28 @@
 import express from "express";
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore, Timestamp } from "firebase-admin/firestore";
-import handleQuery from "./handlers/query.js";
+//import { initializeApp, cert } from "firebase-admin/app";
+//import { getFirestore, Timestamp } from "firebase-admin/firestore";
+//import handleQuery from "./handlers/query.js";
 
-initializeApp({
-  credential: cert(JSON.parse(process.env.GCP_SERVICE_ACCOUNT)),
-});
-const db = getFirestore();
+//initializeApp({
+//  credential: cert(JSON.parse(process.env.GCP_SERVICE_ACCOUNT)),
+//});
+//const db = getFirestore();
 
 const app = express();
-app.use(express.json());
-app.get("/query", async (req, res) => await handleQuery(req, res));
-app.get("/listing", async (req, res) => await handleListing(req, res));
-app.get(
-  "/lising-images",
-  async (req, res) => await handleListingImages(req, res),
-);
-app.post("parse", async (req, res) => await handleParse(req, res));
-app.get("/", async (req, res) => await handleRoot(req, res));
+//app.use(express.json());
+//app.get("/query", async (req, res) => await handleQuery(req, res));
+//app.get("/listing", async (req, res) => await handleListing(req, res));
+//app.get(
+//  "/lising-images",
+//  async (req, res) => await handleListingImages(req, res),
+//);
+//app.post("parse", async (req, res) => await handleParse(req, res));
+//app.get("/", async (req, res) => await handleRoot(req, res));
+
+app.get('/', async (req, res) => {
+
+  res.text('kk').send()
+})
 
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT);
