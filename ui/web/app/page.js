6aecf5dedcdec18244ef328f32d9dabaeb19419c
fullname <fullname@fullnames-MacBook-Pro.local>
2023-12-28 23:18:31 -0800

    i

--- ui/web/app/page.js
+++ ui/web/app/page.js
@@ -33,7 +33,7 @@ export default async function Page() {
 
   return (
     <>
-      <Index places={places} sublicenseInitialData={{places}} />
+      <Index places={places} sublicenseInitialData={places} />
     </>
   );
 }
