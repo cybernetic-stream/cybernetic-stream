3ef747ab918e29bfd3a88ad5d36dd6c3a4037cc4
cybernetic-stream <microsoft@baronbegier.com>
2024-01-07 20:02:08 -0800

    i

--- data/fleet/Sourcing/lib/getPuppeteer.js
+++ data/fleet/Sourcing/lib/getPuppeteer.js
@@ -6,6 +6,7 @@ export default async function getPuppeteer() {
   return await puppeteer.launch(
     process.env.IS_CONTAINER
       ? {
+          headless: 'new',
           executablePath: "/usr/bin/google-chrome",
           args: ["--no-sandbox", "--disable-setuid-sandbox"],
         }
