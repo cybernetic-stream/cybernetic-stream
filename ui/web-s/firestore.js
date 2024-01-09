d42c0223918643167facec253d16196fb16aa612
firstname lastname <devops@baronbegier.com>
2024-01-09 13:46:08 -0800

    i

--- ui/web-s/firestore.js
+++ ui/web-s/firestore.js
@@ -1,16 +1,21 @@
 import { cert, getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-let serviceAccount = process.env.GCP_SERVICE_ACCOUNT;
+let serviceAccount;
 
 if (process.env.IS_DEPLOYMENT) {
-  serviceAccount = Buffer.from(serviceAccount, 'base64').toString('ascii');
+  // Decode from base64 when IS_DEPLOYMENT is set
+  serviceAccount = Buffer.from(process.env.BASE64_GCP_SERVICE_ACCOUNT, 'base64').toString('ascii');
+} else {
+  // Directly use the GCP_SERVICE_ACCOUNT variable when IS_DEPLOYMENT is not set
+  serviceAccount = process.env.GCP_SERVICE_ACCOUNT;
 }
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(JSON.parse(serviceAccount)),
+    credential: cert(JSON.parse(serviceAccount))
   });
 }
+
 
 export const db = getFirestore();
